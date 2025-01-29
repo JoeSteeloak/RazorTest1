7e2c3091c7069b0d20757bf18b1a3161f99d17d9
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-01-29 15:07:30 +0100

    work on hamburger menu

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Funktion för att toggla menyn
+function toggleMenu() {
+    const navMenu = document.getElementById("navMenu");
+    navMenu.classList.toggle("open"); // Lägg till eller ta bort klassen "open"
+}
